7db0805ad7110b3a146fa14a50ba7023e2b0d317
Braden Collins <Bradencollins208@gmail.com>
2026-03-23 12:54:20 -0400

    started on a board class with a function to print the board and take the users choice as X or O

--- Lab 9.cpp
+++ Lab 9.cpp
@@ -7,8 +7,76 @@
 #include <string>
 using namespace std;
 
+class board{
+	public:
+		string topLeft;
+		string topMiddle;
+		string topRight;
+		
+		string middleLeft;
+		string middleMiddle;
+		string middleRight;
+		
+		string bottomLeft;
+		string bottomMiddle;
+		string bottomRight;
+		
+		
+		void printBoard(){
+			
+			topLeft="\n 1 ";
+			topMiddle="| 2 |";
+			topRight=" 3 ";
+			
+			middleLeft=" 4 ";
+			middleMiddle="| 5 |";
+			middleRight=" 6 ";
+			
+			bottomLeft=" 7 ";
+			bottomMiddle="| 8 |";
+			bottomRight=" 9 \n";
+			
+			
+			cout<<topLeft;
+			cout<<topMiddle;
+			cout<<topRight<<endl;
+			
+			cout<<"-----------"<<endl;
+			
+			cout<<middleLeft;
+			cout<<middleMiddle;
+			cout<<middleRight<<endl;
+			
+			cout<<"-----------"<<endl;
+			
+			cout<<bottomLeft;
+			cout<<bottomMiddle;
+			cout<<bottomRight<<endl;
+		}
+};
+
+
 int main(){
-	cout<<"hello world\n";
+	board newBoard;
 	
+	int XorO=0;
+	
+	cout<<"Tic Tac Toe"<<endl;
+	cout<<"1) X"<<endl;
+	cout<<"2) O"<<endl;
+	cout<<"What would like to play: ";
+	cin>>XorO;
+	
+	if(XorO==1){
+		cout<<"User is X"<<endl;
+	}
+	else if(XorO==2){
+		cout<<"User is O"<<endl;
+	}
+	else{
+		cout<<"Invalid Input"<<endl;
+	}
+	
+	newBoard.printBoard();
 	return 0;
 }//end main()
