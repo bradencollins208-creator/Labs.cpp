f18fa891ddc3b5189886cb3f3417de022bea380a
Braden Collins <Bradencollins208@gmail.com>
2026-03-25 11:47:43 -0400

    added a new function to the board class to check when the user is close to a win and worked on making the computer block them if so

--- Lab 9.cpp
+++ Lab 9.cpp
@@ -71,7 +71,7 @@ class board{
 			}//end else if
 			
 			//middle row
-			else if(middleLeft==p && middleMiddle==p && bottomMiddle==p){
+			else if(middleLeft==p && middleMiddle==p && middleRight==p){
 				return true;
 			}//end else if
 			
@@ -95,6 +95,104 @@ class board{
 				//
 			}//end else
 		}//end winCondition()
+		
+		//returns true if the space is open
+		bool checkSpace(string space){
+			if(space==" 1 " || space==" 2 " || space== " 3 "
+			|| space==" 4 " || space==" 5 " || space==" 6 " 
+			|| space==" 7 " || space==" 8 " || space==" 9 "){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}//end checkSpace()
+		
+		int checkCloseWin(string p){
+			//top left column
+			if(topLeft==p && middleLeft==p){
+				return 1;
+			}//end if
+			
+			//top middle column
+			else if(topMiddle==p && middleMiddle==p){
+				return 2;
+			}//end else if
+			
+			//top right column
+			else if(topRight==p && middleRight==p){
+				return 3;
+			}//end else if
+			
+			//bottom left column
+			else if(middleLeft==p && bottomLeft==p){
+				return 4;
+			}//end if
+			
+			//bottom middle column
+			else if(middleMiddle==p && bottomMiddle==p){
+				return 5;
+			}//end else if
+			
+			//bottom right column
+			else if(middleRight==p && bottomRight==p){
+				return 6;
+			}//end else if
+			
+			//top left row
+			else if(topLeft==p && topMiddle==p){
+				return 7;
+			}//end else if
+			
+			//middle left row
+			else if(middleLeft==p && middleMiddle==p){
+				return 8;
+			}//end else if
+			
+			//bottom left row
+			else if(bottomLeft==p && bottomMiddle==p){
+				return 9;
+			}//end else if
+			
+			//top right row
+			else if(topMiddle==p && topRight==p){
+				return 10;
+			}//end else if
+			
+			//middle right row
+			else if(middleMiddle==p && bottomMiddle==p){
+				return 11;
+			}//end else if
+			
+			//bottom tight row
+			else if(bottomMiddle==p && bottomRight==p){
+				return 12;
+			}//end else if
+			
+			//bottom forward slash
+			else if(bottomLeft==p && middleMiddle==p){
+				return 13;
+			}//end else if
+			
+			//top forward slash
+			else if(middleMiddle==p && topRight==p){
+				return 14;
+			}//end else if
+			
+			//top back slash
+			else if(topLeft==p && middleMiddle==p){
+				return 15;
+			}//end else if
+			
+			//bottom back slash
+			else if(middleMiddle==p && bottomRight==p){
+				return 16;
+			}//end else if
+			
+			else{
+				return 0;
+			}//end else
+		}//end checkCloseWin()
 };//end class board
 
 
@@ -128,9 +226,105 @@ int main(){
 			newBoard.printBoard();
 			cout<<"Enter the number of the space you would like to play: ";
 			cin>>spaceChoice;
-			
-			//newBoard.middleMiddle=" X ";
-		}
+			if(spaceChoice==1){
+				if(newBoard.checkSpace(newBoard.topLeft)==true){
+					newBoard.topLeft=" X ";
+					if(newBoard.checkCloseWin(" X ")==1){
+						newBoard.bottomLeft=" O ";
+					}
+					else if(newBoard.checkCloseWin(" X ")==7){
+						newBoard.topRight=" O ";
+					}
+					else if(newBoard.checkCloseWin(" X ")==15){
+						newBoard.bottomRight=" O ";
+					}
+					else if(newBoard.checkCloseWin(" X ")==0){
+						newBoard.topMiddle=" O ";
+					}
+				}
+				else{
+					cout<<"\nSpace filled\nEnter another number"<<endl;
+				}
+			}
+			else if(spaceChoice==2){
+				if(newBoard.checkSpace(newBoard.topMiddle)==true){
+					newBoard.topMiddle=" X ";
+					if(newBoard.checkCloseWin(" X ")==2){
+						newBoard.bottomMiddle=" O ";
+					}
+					else if(newBoard.checkCloseWin(" X ")==7){
+						newBoard.topRight=" O ";
+					}
+					else if(newBoard.checkCloseWin(" X ")==10){
+						newBoard.topLeft=" O ";
+					}
+					else if(newBoard.checkCloseWin(" X ")==0){
+						newBoard.middleMiddle=" O ";
+					}
+				}
+				else{
+					cout<<"\nSpace filled\nEnter another number"<<endl;
+				}
+			}
+			else if(spaceChoice==3){
+				if(newBoard.checkSpace(newBoard.topRight)==true){
+					newBoard.topRight=" X ";
+				}
+				else{
+					cout<<"\nSpace filled\nEnter another number"<<endl;
+				}
+			}
+			else if(spaceChoice==4){
+				if(newBoard.checkSpace(newBoard.middleLeft)==true){
+					newBoard.middleLeft=" X ";
+				}
+				else{
+					cout<<"\nSpace filled\nEnter another number"<<endl;
+				}
+			}
+			else if(spaceChoice==5){
+				if(newBoard.checkSpace(newBoard.middleMiddle)==true){
+					newBoard.middleMiddle=" X ";
+				}
+				else{
+					cout<<"\nSpace filled\nEnter another number"<<endl;
+				}
+			}
+			else if(spaceChoice==6){
+				if(newBoard.checkSpace(newBoard.middleRight)==true){
+					newBoard.middleRight=" X ";
+				}
+				else{
+					cout<<"\nSpace filled\nEnter another number"<<endl;
+				}
+			}
+			else if(spaceChoice==7){
+				if(newBoard.checkSpace(newBoard.bottomLeft)==true){
+					newBoard.bottomLeft=" X ";
+				}
+				else{
+					cout<<"\nSpace filled\nEnter another number"<<endl;
+				}
+			}
+			else if(spaceChoice==8){
+				if(newBoard.checkSpace(newBoard.bottomMiddle)==true){
+					newBoard.bottomMiddle=" X ";
+				}
+				else{
+					cout<<"\nSpace filled\nEnter another number"<<endl;
+				}
+			}
+			else if(spaceChoice==9){
+				if(newBoard.checkSpace(newBoard.bottomRight)==true){
+					newBoard.bottomRight=" X ";
+				}
+				else{
+					cout<<"\nSpace filled\nEnter another number"<<endl;
+				}
+			}
+		}//end while loop
+		newBoard.printBoard();
+		cout<<"X wins"<<endl;
 	}
 	else if(XorO==2){
 		cout<<"User is O"<<endl;
