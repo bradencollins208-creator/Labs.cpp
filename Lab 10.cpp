fc73b5444dd0016b0d3ddcba73b15f887edf035a
Braden Collins <Bradencollins208@gmail.com>
2026-04-06 11:18:17 -0400

    Added score tracking a comments

--- Lab 10.cpp
+++ Lab 10.cpp
@@ -151,147 +151,192 @@ class board3D: public board{
 				return true;
 			}//end if
 			
+			//return false if no above condition is met
 			return false;
 		}//end winCondition()
 };//end board3D
 
 int main(){
-	//generates random number
+	//seeds for random numbers
 	srand(time(0));
-	int randomNum=rand()%2+1;
 	
-	//creates varibles for the user, computer, and the turn the game is on
-	string user;
-	string computer;
-	bool userTurn;
-	
-	//sets the user as X or O and the turn the game starts on
-	if(randomNum==1){
-		user=" X ";
-		computer=" O ";
-		userTurn=true;
-	}//end if
-	else{
-		user=" O ";
-		computer=" X ";
-		userTurn=false;
-	}//end else
-	
-	//outputs what the user and computer is
-	cout<<"User is:"<<user<<"\nComputer is:"<<computer<<endl;
-	
-	//creates a board instance
+	//creates a board3D instance
 	board3D newBoard;
 	
-	//sets the boards spaces to be different numbers
-	newBoard.board3D[0][0][0]=" 1 ";
-	newBoard.board3D[0][0][1]=" 2 ";
-	newBoard.board3D[0][0][2]=" 3 ";
+	//creates score variables for the user and computer
+	int userScore=0;
+	int compScore=0;
 	
-	newBoard.board3D[0][1][0]=" 4 ";
-	newBoard.board3D[0][1][1]=" 5 ";
-	newBoard.board3D[0][1][2]=" 6 ";
+	//creates a choice variable for the user
+	int iChoice=1;
 	
-	newBoard.board3D[0][2][0]=" 7 ";
-	newBoard.board3D[0][2][1]=" 8 ";
-	newBoard.board3D[0][2][2]=" 9 ";
-	
-	
-	newBoard.board3D[1][0][0]=" 10 ";
-	newBoard.board3D[1][0][1]=" 11 ";
-	newBoard.board3D[1][0][2]=" 12 ";
-	
-	newBoard.board3D[1][1][0]=" 13 ";
-	newBoard.board3D[1][1][1]=" 14 ";
-	newBoard.board3D[1][1][2]=" 15 ";
-
-	newBoard.board3D[1][2][0]=" 16 ";
-	newBoard.board3D[1][2][1]=" 17 ";
-	newBoard.board3D[1][2][2]=" 18 ";
-	
-	
-	newBoard.board3D[2][0][0]=" 19 ";
-	newBoard.board3D[2][0][1]=" 20 ";
-	newBoard.board3D[2][0][2]=" 21 ";
-	
-	newBoard.board3D[2][1][0]=" 22 ";
-	newBoard.board3D[2][1][1]=" 23 ";
-	newBoard.board3D[2][1][2]=" 24 ";
-	
-	newBoard.board3D[2][2][0]=" 25 ";
-	newBoard.board3D[2][2][1]=" 26 ";
-	newBoard.board3D[2][2][2]=" 27 ";
-	
-	//creates a variable to count the number of moves made
-	int moves=0;
-	
-	//loops the game until either the user or computer wins or the move count gets to 27 or more
-	while(!newBoard.winCondition3D(user) && !newBoard.winCondition3D(computer) && moves<27){
+	//runs a while loop while until the user enters 0
+	while(iChoice!=0){
+		//creates a menu with options for the user
+		cout<<"\nMenu"<<endl;
+		cout<<"(1) New Game"<<endl;
+		cout<<"(0) Exit"<<endl;
 		
-		newBoard.printBoard3D();
+		//takes the users input
+		cout<<"What do you want to do: ";
+		cin>>iChoice;
 		
-		if(userTurn==true){
-			//takes an input from the user for where they want to play
-			int spaceChoice;
-			cout<<"\nEnter the number of the space you would like to play: ";
-			cin>>spaceChoice;
+		//checks that the choice was between 0 and 1
+		if(iChoice<0 || iChoice>1){
+			cout<<"Invalid number\n";
+			continue;
+		}//end if
+		
+		//generates a random number
+		int randomNum=rand()%2+1;
+		
+		//creates varibles for the user, computer, and the turn the game is on
+		string user;
+		string computer;
+		bool userTurn;
+		
+		//sets the user as X or O and the turn the game starts on
+		if(randomNum==1){
+			user=" X ";
+			computer=" O ";
+			userTurn=true;
+		}//end if
+		else{
+			user=" O ";
+			computer=" X ";
+			userTurn=false;
+		}//end else
+		
+		//runs if the users chooses 1
+		if(iChoice==1){
+			//outputs what the user and computer is
+			cout<<"\nUser is:"<<user<<"\nComputer is:"<<computer<<endl;
 			
-			//checks that the choice was between 1 and 27
-			if(spaceChoice<1 || spaceChoice>27){
-				cout<<"Invalid number\n";
-				continue;
+			//outputs the scores for the user and computer
+			cout<<"\nUser score: "<<userScore<<endl;
+			cout<<"Computer score: "<<compScore<<endl;
+		
+			//sets the boards spaces to be different numbers
+			newBoard.board3D[0][0][0]=" 1 ";
+			newBoard.board3D[0][0][1]=" 2 ";
+			newBoard.board3D[0][0][2]=" 3 ";
+			
+			newBoard.board3D[0][1][0]=" 4 ";
+			newBoard.board3D[0][1][1]=" 5 ";
+			newBoard.board3D[0][1][2]=" 6 ";
+			
+			newBoard.board3D[0][2][0]=" 7 ";
+			newBoard.board3D[0][2][1]=" 8 ";
+			newBoard.board3D[0][2][2]=" 9 ";
+			
+			
+			newBoard.board3D[1][0][0]=" 10 ";
+			newBoard.board3D[1][0][1]=" 11 ";
+			newBoard.board3D[1][0][2]=" 12 ";
+			
+			newBoard.board3D[1][1][0]=" 13 ";
+			newBoard.board3D[1][1][1]=" 14 ";
+			newBoard.board3D[1][1][2]=" 15 ";
+		
+			newBoard.board3D[1][2][0]=" 16 ";
+			newBoard.board3D[1][2][1]=" 17 ";
+			newBoard.board3D[1][2][2]=" 18 ";
+			
+			
+			newBoard.board3D[2][0][0]=" 19 ";
+			newBoard.board3D[2][0][1]=" 20 ";
+			newBoard.board3D[2][0][2]=" 21 ";
+			
+			newBoard.board3D[2][1][0]=" 22 ";
+			newBoard.board3D[2][1][1]=" 23 ";
+			newBoard.board3D[2][1][2]=" 24 ";
+			
+			newBoard.board3D[2][2][0]=" 25 ";
+			newBoard.board3D[2][2][1]=" 26 ";
+			newBoard.board3D[2][2][2]=" 27 ";
+			
+			//creates a variable to count the number of moves made
+			int moves=0;
+			
+			//loops the game until either the user or computer wins or the move count gets to 27 or more
+			while(!newBoard.winCondition3D(user) && !newBoard.winCondition3D(computer) && moves<27){
+				//prints the board
+				newBoard.printBoard3D();
+				
+				//runs if the game is on the users turn
+				if(userTurn==true){
+					//takes an input from the user for where they want to play
+					int spaceChoice;
+					cout<<"\nEnter the number of the space you would like to play: ";
+					cin>>spaceChoice;
+					
+					//checks that the choice was between 1 and 27
+					if(spaceChoice<1 || spaceChoice>27){
+						cout<<"Invalid number\n";
+						continue;
+					}//end if
+					
+					//converts the number entered by the user into row, column, and layer values for the 3d board array
+					int row=((spaceChoice-1)/3)%3;
+					int col=(spaceChoice-1)%3;
+					int lay=(spaceChoice-1)/9;
+					
+					//checks if a space the user wants to play is occupied
+					if(newBoard.board3D[lay][row][col]!=user && newBoard.board3D[lay][row][col]!=computer){
+						newBoard.board3D[lay][row][col]=user;//sets a players move to the space they choose if it is open
+						moves++;//increases the move counter
+						userTurn=false;//switches the turn so the computer can play
+					}//end if
+					
+					//makes the user choose another number if they choose an occupied spot
+					else{
+						cout<<"\nSpace filled\nEnter another number"<<endl;
+					}//end else
+				}//end if
+				
+				//runs if the game is on the computers turn
+				else{
+					//creates variables for the computers move and its row, column, and layer choice
+					int compMove;
+					int r;
+					int c;
+					int l;
+					
+					//runs a do while loop to get the row, column, and layer value for the computers move
+					do{
+						compMove=rand()%27+1;
+						r=((compMove-1)/3)%3;
+						c=(compMove-1)%3;
+						l=(compMove-1)/9;
+					}while(newBoard.board3D[l][r][c]==user || newBoard.board3D[l][r][c]==computer);//end do while loop
+					newBoard.board3D[l][r][c]=computer;//sets the computers move the random space if it is open
+					cout<<"Computer plays: "<<compMove<<endl;//outputs the computers move
+					moves++;//increases the move counter
+					userTurn=true;//switches the turn so the user can play
+				}//end else
+			}//end while loop
+			
+			//checks if either the user or computer won or there was a draw and outputs the appropriate output
+			newBoard.printBoard3D();
+			
+			if(newBoard.winCondition3D(user)==true){
+				cout<<"\n"<<user<<"wins"<<endl;
+				userScore++;
 			}//end if
-			
-			//converts the number entered by the user into row, column, and layer values for the 3d board array
-			int row=((spaceChoice-1)/3)%3;
-			int col=(spaceChoice-1)%3;
-			int lay=(spaceChoice-1)/9;
-			
-			//checks if a space the user wants to play is occupied
-			if(newBoard.board3D[lay][row][col]!=user && newBoard.board3D[lay][row][col]!=computer){
-				newBoard.board3D[lay][row][col]=user;//sets a players move to the space they choose if it is open
-				moves++;//increases the move counter
-				userTurn=false;//switches the turn so the computer can play
-			}//end if
-			
-			//makes the user choose another number if they choose an occupied spot
+			else if(newBoard.winCondition3D(computer)==true){
+				cout<<"\n"<<computer<<"wins"<<endl;
+				compScore++;
+			}//end else if
 			else{
-				cout<<"\nSpace filled\nEnter another number"<<endl;
+				cout<<"\nDraw"<<endl;
 			}//end else
 		}//end if
 		
+		//exits the program if the user chooses 0
 		else{
-			//creates variables for the computers move and its row, column, and layer choice
-			int compMove;
-			int r;
-			int c;
-			int l;
-			
-			//runs a do while loop to get the row, column, and layer value for the computers move
-			do{
-				compMove=rand()%27+1;
-				r=((compMove-1)/3)%3;
-				c=(compMove-1)%3;
-				l=(compMove-1)/9;
-			}while(newBoard.board3D[l][r][c]==user || newBoard.board3D[l][r][c]==computer);//end do while loop
-			newBoard.board3D[l][r][c]=computer;//sets the computers move the random space if it is open
-			cout<<"Computer plays: "<<compMove<<endl;//outputs the computers move
-			moves++;//increases the move counter
-			userTurn=true;//switches the turn so the user can play
+			cout<<"\nUser score: "<<userScore<<endl;
+			cout<<"Computer score: "<<compScore<<endl;
 		}//end else
 	}//end while loop
-	
-	newBoard.printBoard3D();
-	
-	if(newBoard.winCondition3D(user)==true){
-		cout<<"\n"<<user<<"wins"<<endl;
-	}//end if
-	else if(newBoard.winCondition3D(computer)==true){
-		cout<<"\n"<<computer<<"wins"<<endl;
-	}//end else if
-	else{
-		cout<<"\nDraw"<<endl;
-	}//end else
-	
 	return 0;
 }//end main()
